1ec0754efbaa45e7ed5b74460bfb2e9dd6423578
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2018-03-14 21:16:59 +0300

    Save contacts

--- app/src/main/java/home/work/pcconfig/ui/hab/screen/stream/StreamMessageScreen.java
+++ app/src/main/java/home/work/pcconfig/ui/hab/screen/stream/StreamMessageScreen.java
@@ -7,10 +7,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
 import com.github.mrvilkaman.presentationlayer.fragments.core.BaseFragment;
+import com.github.mrvilkaman.presentationlayer.utils.ui.UIUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,8 +22,8 @@ import home.work.pcconfig.R;
 
 public class StreamMessageScreen extends BaseFragment<StreamMessagePresenter> implements StreamMessageView {
 
-    private static final int PICK_CONTACT = 123123;
     @BindView(R.id.phonenumber) EditText phoneEdit;
+    @BindView(R.id.choose_contact_add) View addContact;
 
     public static StreamMessageScreen newInstance() {
         Bundle args = new Bundle();
@@ -38,19 +41,35 @@ public class StreamMessageScreen extends BaseFragment<StreamMessagePresenter> im
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         ButterKnife.bind(this,view);
         super.onViewCreated(view, savedInstanceState);
+
+
+        phoneEdit.addTextChangedListener(new HideButtonTextWhater());
     }
 
     @Override
     public void bindPhone(String number) {
         phoneEdit.setText(number);
+        UIUtils.changeVisibility(addContact,false);
     }
 
     @OnClick(R.id.choose_contact)
     void onClickChooseContact(){
+
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         // BoD con't: CONTENT_TYPE instead of CONTENT_ITEM_TYPE
         intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
         startActivityForResult(intent, 1);
+    }
+
+    @OnClick(R.id.choose_contact_add)
+    void onClickAddContact(){
+        Intent intent = new Intent(Intent.ACTION_INSERT);
+        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+
+//        intent.putExtra(ContactsContract.Intents.Insert.NAME, UIUtils.asString(phoneEdit));
+        intent.putExtra(ContactsContract.Intents.Insert.PHONE, UIUtils.asString(phoneEdit));
+//        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, person.email);
+        startActivity(intent);
     }
 
     @Override
@@ -81,5 +100,22 @@ public class StreamMessageScreen extends BaseFragment<StreamMessagePresenter> im
             }
         }
 
+    }
+
+    private class HideButtonTextWhater implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            UIUtils.changeVisibility(addContact,s.length() != 0);
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
     }
 }
